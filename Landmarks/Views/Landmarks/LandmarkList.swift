864db6398cee660fd059a2354722a7d193e80213
Cris Messias <meunomeecriss@gmail.com>
2025-02-07 15:09:38 +0100

    Handling user Input

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Cris Messias on 07/02/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { eachLandmark in
+            (!showFavoritesOnly || eachLandmark.isFavorite )
+        }
+    }
+
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
